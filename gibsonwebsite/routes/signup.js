e6c6bf6efd50b9e06336af44fb173526dff86c10
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2016-02-15 19:37:34 -0500

    Unreverted sign up page

--- gibsonwebsite/routes/signup.js
+++ gibsonwebsite/routes/signup.js
@@ -5,14 +5,14 @@ var router = express.Router();
 module.exports = function(passport){
 
 //load sign up page
-router.get('/', function(req,res,next){
+router.get('/signup', function(req,res,next){
 
 	res.render('signup', { title: 'Sign Up'});
 	
 });
 
 //create new user
-router.post('/',  passport.authenticate('local-signup', {
+router.post('/signup',  passport.authenticate('local-signup', {
 	successRedirect: '/index',		// Redirect to main page when login complete
 	failureRedirect: '/lol',	// Return to login when fail, and flash error
 	failureFlash: true
