ccd4e4ef0354b1d8aad16f08402c1075fcb245d5
Rayden440 <kevinxu.444@gmail.com>
2016-02-17 23:37:05 -0500

    Added exception for CSP (needs testing)

--- gibsonwebsite/app.js
+++ gibsonwebsite/app.js
@@ -22,9 +22,17 @@ app.set('view engine', 'ejs');
 // Security
 app.use(helmet());
 app.use(helmet.csp({
-  defaultSrc: ["'self'"],
-  scriptSrc: ['/public/js/*'],
-  styleSrc: ['/pubic/css/*'],
+  defaultSrc: ['self'],
+  scriptSrc: ['http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js',  // signup.ejs, SuccessSignup.ejs, login.ejs
+              'https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js',  // SuccessSignup.ejs
+              'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', // SuccessSignup.ejs
+              'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',  // SuccessSignup.ejs
+
+             ],
+  styleSrc: ['http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // SuccessSignup.ejs
+             'http://fonts.googleapis.com/css?family=Montserrat:400,700',  // standardnavbar.ejs
+             'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic',  // standardnavbar.ejs
+            ],
   imgSrc: [],
   connectSrc: ["'none'"],
   fontSrc: ['/public/font-awesome/css/*'],
