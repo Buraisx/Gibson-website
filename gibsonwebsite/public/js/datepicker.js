81fe88320720b6030c8a207ff2e4ce1e2ee81bbe
Leo <ienzoh@live.com>
2016-02-17 20:39:34 -0500

    Created range for valid birthdates

--- gibsonwebsite/public/js/datepicker.js
+++ gibsonwebsite/public/js/datepicker.js
@@ -1,5 +1,6 @@
 $(function() {
     $('#datepicker').datepicker({
-        format: 'yyyy/mm/dd'
+        format: 'yyyy/mm/dd',
+        startDate: '1900/01/01' // The earliest date that can be chosen
     });
 });
