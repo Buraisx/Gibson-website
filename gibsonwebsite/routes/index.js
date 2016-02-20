9c032dc2fc1aeaa7e0a0a4975068b12839408b93
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2016-02-20 12:45:15 -0500

    Added philosophy route

--- gibsonwebsite/routes/index.js
+++ gibsonwebsite/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gibson' });
 });
 
+router.get('/philosophy', function(req, res, next) {
+  res.render('Philosophy', { title: 'Gibson' });
+});
+
 module.exports = router;
