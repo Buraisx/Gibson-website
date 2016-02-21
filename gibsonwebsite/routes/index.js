320357abd91ab665aad8e5040aa2d9757341b337
Daniel Liu <hsiu.liu@uoit.net>
2016-02-20 20:59:54 -0500

    Static pages update
    media centre page is broken

--- gibsonwebsite/routes/index.js
+++ gibsonwebsite/routes/index.js
@@ -6,8 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gibson' });
 });
 
+router.get('/aboutus', function(req, res, next) {
+  res.render('Aboutus', { title: 'Gibson' });
+});
+
 router.get('/philosophy', function(req, res, next) {
   res.render('Philosophy', { title: 'Gibson' });
 });
+
+router.get('/theteam', function(req, res, next) {
+  res.render('Theteam', { title: 'Gibson' });
+});
+
+router.get('/financials', function(req, res, next) {
+  res.render('Financials', { title: 'Gibson' });
+});
+
+router.get('/mediacentre', function(req, res, next) {
+  res.render('Mediacentre', { title: 'Gibson' });
+});
+
+router.get('/hours', function(req, res, next) {
+  res.render('Hours', { title: 'Gibson' });
+});
+
+router.get('/programs', function(req, res, next) {
+  res.render('Programs', { title: 'Gibson' });
+});
+
+router.get('/community', function(req, res, next) {
+  res.render('Community', { title: 'Gibson' });
+});
+
+router.get('/theteam', function(req, res, next) {
+  res.render('Theteam', { title: 'Gibson' });
+});
+
+
 
 module.exports = router;
