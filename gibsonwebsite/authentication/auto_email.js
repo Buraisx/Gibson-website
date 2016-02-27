37759597d72169175e3ea04853951a0b98fbe28e
Rayden440 <kevinxu.444@gmail.com>
2016-02-26 19:57:38 -0500

    email confirmation 90%, awaiting changes to signup so that users are made in temporary table

--- gibsonwebsite/authentication/auto_email.js
+++ gibsonwebsite/authentication/auto_email.js
@@ -8,22 +8,30 @@ var transport = nodemailer.createTransport(smtpTransport(config.transport));
 // SENDING EMAIL
 function signupConfEmail (req, res, next){
 
-  // CONFIGURING E-MAIL
-  var mailOptions = {
-    from: '"105 Gibson" <nansagad@gmail.com>',
-    to: 'kevinxu_95@hotmail.com',
-    subject: '105 Gibson Confirm Your Email Address',
-    text: "Here's your token: " +req.oneUseToken,
-  };
-
-  transport.sendMail(mailOptions, function(error, info){
-    if(error){
-      console.log(error);
-      return;
-    }
-    console.log('Message sent: ' + info.response);
+  // IF config.sendEmail = false, DISABLE OUTGOING EMAIL
+  if (!config.sendEmail){
     next();
-  });
+  }
+  // config.sendEmail = true, DO SEND EMAIL
+  else{
+
+    // CONFIGURING E-MAIL
+    var mailOptions = {
+      from: '"105 Gibson" <nansagad@gmail.com>',
+      to: 'kevinxu_95@hotmail.com',
+      subject: '105 Gibson Center - Please Confirm Your Email Address',
+      text: "Please go to the following web address to confirm your email address: \n https://localhost:3000/confirm/token/" +req.oneUseToken,
+    };
+
+    transport.sendMail(mailOptions, function(error, info){
+      if(error){
+        console.log(error);
+        return;
+      }
+      console.log('Message sent: ' + info.response);
+      next();
+    });
+  }
 }
 
 module.exports.signupConfEmail = signupConfEmail;
