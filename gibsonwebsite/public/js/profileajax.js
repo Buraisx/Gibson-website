82169909aa657a42a28b6140ee0afad2dd98c573
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2016-02-28 00:41:42 -0500

    Some ajax features work on course tab

--- gibsonwebsite/public/js/profileajax.js
+++ gibsonwebsite/public/js/profileajax.js
@@ -20,14 +20,23 @@ $("a[href$='#courses']").click(function() {
 	
 		for(i = 0; i < data.length; i++){
 			
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-			document.getElementById("description").innerHTML = "lol";
-		}	
+			document.getElementById("coursename").innerHTML = "Course Name: " + data[0].course_name;
+			document.getElementById("coursename").coursename = "Course Name: " + data[0].course_name;
+			alert(document.getElementById("coursename").coursename);
+			document.getElementById("courseid").innerHTML = "Course ID: "+ data[0].course_id;
+			document.getElementById("courseid").courseid = "Course ID: "+ data[0].course_id;
+			alert(document.getElementById("courseid").courseid);
+
+			document.getElementById("description").innerHTML = "Description: " + data[0].course_description;
+			document.getElementById("coursestartdate").innerHTML = "Start Date: "+ data[0].start_date;
+			document.getElementById("courseenddate").innerHTML = "End Date: " + data[0].end_date;
+			document.getElementById("coursetime").innerHTML = "Course Time: " + data[0].course_time;
+			document.getElementById("courseinterval").innerHTML = "Course Interval: " + data[0].course_interval;
+			document.getElementById("coursedays").innerHTML = "Course Days: " + data[0].course_days;
+			document.getElementById("coursetarget").innerHTML = "Course target: " + data[0].course_target;
+			document.getElementById("coursecost").innerHTML = "Course cost: " + data[0].default_fee;
+			
+		}
 				
 	});
 });
