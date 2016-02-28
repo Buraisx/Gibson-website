cae972b0701433188df8cb36127d9407a8f68dc0
Benjamin Zhao <Benjamin.zhao1995@hotmail.com>
2016-02-28 17:36:35 -0500

    made the profile courses html generated from jQuery -profileajax.js

--- gibsonwebsite/public/js/profileajax.js
+++ gibsonwebsite/public/js/profileajax.js
@@ -40,6 +40,7 @@ $("a[href$='#courses']").click(function() {
 			document.getElementById("coursecost").innerHTML = "Course cost: " + data[0].default_fee;
 			*/
 			var panel_default = $("<div></div>", {class: "panel panel-default"});
+			
 			var panel_heading = $("<div></div>", {class: "panel-heading"});
 			var panel_title = $("<h4></h4>", {class: "panel-title"});
 			var collapse = $("<a></a>", {href: "#collapse"+i});
@@ -47,8 +48,10 @@ $("a[href$='#courses']").click(function() {
 			collapse.attr("data-parent", "#accordion");
 			var coursename = $("<p></p>", {id: "coursename"}).append("Course Name:", data[i].course_name);
 			var courseid = $("<p></p>", {id: "courseid"}).append("Course ID:", data[i].course_id);
+
 			var collapse2 = $("<div></div>", {id: "collapse" + i, class: "panel-collapse collapse in"});
 			var panelbody = $("<div></div>", {class: "panel-body"});
+
 			var row = $("<div></div>", {class: "row"});
 			var colsm6 = $("<div></div>", {class:"form-group col-sm-6"});
 			var description = $("<p></p>", {id: "description"}).append("Description: ").append(data[i].course_description);
@@ -61,39 +64,54 @@ $("a[href$='#courses']").click(function() {
 			var coursecost = $("<p></p>", {id: "cost"}).append("Cost: $" + data[0].default_fee);
 			var button = $("<button></button>", {type: "submit", class: "btn btn-default", onclick: "", id:"submit"}).append("Register Now!!");
 
-			//compile the panel
-			collapse = collapse.append(coursename, courseid);
-			panel_title = panel_title.append(collapse);
+
+			//=============================
+			//Top Down compilation hierarchy
+			//=============================
+			panel_default = panel_default.append(panel_heading, collapse2);
+
+			//--------------------------
+			//First div of panel-default
 			panel_heading = panel_heading.append(panel_title);
-			//TODO
-			// prepend colsm6 to all info
-			description = colsm6.append(description);
-			startdate = colsm6.append(startdate);
-			enddate = colsm6.append(enddate);
-			coursetime = colsm6.append(coursetime);
-			courseinterval = colsm6.append(courseinterval);
-			coursedays = colsm6.append(coursedays);
-			coursetarget = colsm6.append(coursetarget);
-			coursecost = colsm6.append(coursecost);
-			button = colsm6.append(button);
+			panel_title = panel_title.append(collapse);
+			collapse = collapse.append(coursename, courseid);
 
-			//TODO organize info into row-groups
+			//--------------------------
+			//Second div of panel-default
+			collapse2 = collapse2.append(panelbody);
 
-			//TODO put into panel_body
+			//DESCRIPTION
+			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		description)));		//escaping closures	
 
-			//TODO put panel_heading and panel_body together
+			panelbody = panelbody.append("<br>");
 
-			row = row.append(colsm6);
-			panelbody = panelbody.append(row, "<br>");
+			//STARTDATE & ENDDATE
+			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		startdate),			//escaping closures
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		enddate)));			//escaping closures
+
+			//COURSE TIME & COURSE INTERVAL
+			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		coursetime),			//escaping closures
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		courseinterval)));			//escaping closures
+
+			panelbody = panelbody.append("<br>");
+
+			//COURSE COST & BUTTON
+			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		coursecost),			//escaping closures
+										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
+										 		button)));			//escaping closures
 
 			
-			//collapse2 = collapse2.append(panelbody);
-
-			panel_default = panel_default.append(panel_heading);
-
-
-
-			$('#4am').after(coursename, courseid, startdate, enddate, button);
+			$('#4am').after(panel_default);
 		}
 
 	});
