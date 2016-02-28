c170db03c3410f0dc115dc9c28ffc97b900c1288
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2016-02-27 21:01:02 -0500

    fixed absolute routes in userprofile.ejs and successSignup.ejs

--- gibsonwebsite/app.js
+++ gibsonwebsite/app.js
@@ -43,9 +43,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 //implement csrf
