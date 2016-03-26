169689c02400d97f7caf9cf8a37eeda727b72bba
JordenZeng <jordenzeng1995@hotmail.com>
2016-03-25 22:44:22 -0400

    made the ajax easier to read

--- gibsonwebsite/public/js/profileajax.js
+++ gibsonwebsite/public/js/profileajax.js
@@ -241,34 +241,28 @@ function load_profile(){
 				"Edit Information"));
 
 		//===============
-		//Change Password
-		var changePassword = $("<div></div>", {class:"row form-group form-group-sm col-sm-12"}).append(
-			$("<button></button>", {type : "button", class: "btn btn-default", onclick:"togglepassworddropdown()", id : "changepassbutton"}).append(
-				"Change Password"));
-
-		//Only client Can change Pass
-		var cpHidden = $("<div></div>", {id:'change_password', class:'hidden changepassdesign form-group form-group-sm row col-sm-12'});
-		var pText = $("<p></p>", {class: "small"}).append("Passwords must contain at least one letter and one number and must have a minimum 6 characters. No special characters.");
-
-		var formInline = $("<form></form>", {class:"form-inline", action:"/user/profile/changepassword", method:"post", role : "form"});
 		var csrf = '<input type = "hidden" name="_csrf" value="'+ $('#_csrf').val() +'">';
+		//Change Password 
+		var changePassword = '';
+		changePassword+='<div class="row form-group form-group-sm col-sm-12">';
+		changePassword+='    <button id="changepassbutton" onclick="togglepassworddropdown()" class="btn btn-default" type="button">Change Password</button>';
+		changePassword+='</div>';
+		changePassword+='<div style="display: block;" class="changepassdesign hidden form-group form-group-sm row col-sm-12" id="change_password">';
+		changePassword+='    <div>';
+		changePassword+='        <p class="small">Passwords must contain at least one letter and one number and must have a minimum 6 characters. No special characters.</p>';
+		changePassword+='        <form role="form" method="post" action="/user/profile/changepassword" class="form-inline" id = "change_password_form">';
+		changePassword+='            <input type = "hidden" name="_csrf" value="'+ $('#_csrf').val() +'">'
+		changePassword+='            <label>Current Password:</label>';
+		changePassword+='            <input type="password" pattern="\\w+" placeholder="Enter Current Password" id="currentpass" name="currentpass" class="form-control">';
+		changePassword+='            <label>New Password:</label>';
+		changePassword+='            <input onkeyup="checkPass(); return false;" minlength="6" placeholder="Enter New Password" id="password" name="newpass" class="form-control" type="password">';
+		changePassword+='            <label>Confirm New Password:</label>';
+		changePassword+='            <input minlength="6" onkeyup="checkPass(); return false;" placeholder="Confirm New Password" id="passwordhashed" name="confirmnewpass" class="form-control" type="password">';
+		changePassword+='            <button id="changepassbutton" class="btn btn-default" type="submit">Change</button>';
+		changePassword+='        </form>';
+		changePassword+='    </div>';
+		changePassword+='</div>';
 
-		//mix in one div form-group
-		var label1 = $("<label></label>").append("Current Password:");
-		var input1 = $("<input></input>", {type:"password", class:"form-control", name:"currentpass", id:"currentpass", placeholder:"Enter Current Password", required:"",
-						pattern:'\\w+'});
-
-		//mix in one div form-group
-		var label2 = $("<label></label>").append("New Password:");
-		var input2 = $("<input></input>", {type : "password", class : "form-control", name : "newpass", id : "password", placeholder:"Enter New Password", minlength: "6",
-										   onkeyup:"checkPass(); return false;", required:""});
-
-		//mix in one div form-group
-		var label3 = $("<label></label>").append("Confirm New Password:");
-		var input3 = $("<input></input>", {type : "password", class : "form-control", name : "confirmnewpass", id : "passwordhashed",
-										   placeholder:"Confirm New Password", onkeyup:"checkPass(); return false;",
-										   minlength: "6", required:""});
-		var button3 = $("<button></button>", {type : "button", class : "btn btn-default", id : "changepassbutton", onClick: "changepassword()"}).append("Change");
 
 		//Add E-Contacts, Edit Info
 		$('#profile').append($("<div></div>", {id: "addecontact", class: "hidden"}).append(newcontact, einputrow1, einputrow2));
@@ -279,22 +273,7 @@ function load_profile(){
 		//===============
 		//Change Password
 		$('#profile').append(changePassword);
-		$('#profile').append(cpHidden);
- 		cpHidden.append($("<div></div>").append(
- 			pText, formInline));
-
- 		//Using Closure Structure
- 		formInline.append(csrf);
-
- 		formInline.append($("<div></div>", {class:"form-group"}).append(
- 			label1, input1));
-
- 		formInline.append($("<div></div>", {class:"form-group"}).append(
- 			label2, input2));
-
- 		formInline.append($("<div></div>", {class:"form-group"}).append(
- 			label3, input3, button3));
-
+		
 		//================
 		$('#change_password_form').validate({
 			rules: {
@@ -491,80 +470,64 @@ function listcourses(){
 
 		$('#courses').contents().remove();
 
-		var courses = $("<div></div>", {class: "panel-group", id: "accordion"});
-
-		for(i = 0; i < data.length; i++){
-			//A Course Accordion Panel
-			var panel_default = $("<div></div>", {class: "panel panel-primary"});
-
-			var panel_heading = $("<div></div>", {class: "panel-heading"});
-			var panel_title = $("<h4></h4>", {class: "panel-title"});
-			var collapse = $("<a></a>", {href: "#collapse"+i});
-			collapse.attr("data-toggle", "collapse");
-			var coursename = $("<div></div>", {class: "col-sm-5"}).append(data[i].course_name);
-			var courseid = $("<div></div>", {class: "col-sm-7 righttext"}).append("Course Code: ", data[i].course_code);
-
-			var collapse2 = $("<div></div>", {id: "collapse" + i, class: "panel-collapse collapse"});
-			var panelbody = $("<div></div>", {class: "panel-body"});
-
-			var row = $("<div></div>", {class: "row"});
-			var colsm6 = $("<div></div>", {class:"form-group col-sm-6"});
-			var description = $("<p></p>", {id: "description"}).append(data[i].course_description);
-			var startdate = $("<p></p>", {id: "coursestartdate"}).append("<strong>Start Date: </strong>" + String(data[i].start_date).substring(0, 10));
-			var enddate = $("<p></p>", {id: "courseenddate"}).append("<strong>End Date: </strong>" + String(data[i].end_date).substring(0, 10));
-			var coursetime = $("<p></p>", {id: "coursetime"}).append("<strong>Time: </strong>" + data[i].course_time);
-			var courseinterval = $("<p></p>", {id: "courseinterval"}).append("<strong>Interval: </strong>" + data[i].course_interval);
-			var coursedays = $("<p></p>", {id: "coursedays"}).append("<strong>Days: </strong>" + data[i].course_days);
-			var coursetarget = $("<p></p>", {id: "coursetarget"}).append("<strong>Target: </strong>" + data[i].course_target);
-			var coursecost = $("<p></p>", {id: "cost"}).append("<strong>Cost: </strong>$" + data[i].default_fee);
-			var button = $("<button></button>", {type: "submit", class: "btn btn-default course-submit", onclick: "register(this)", method:'POST', id:"submit"+data[i].course_id}).append("Register Now!!");
-
-			//=============================
-			//Top Down compilation hierarchy
-			//=============================
-			panel_default = panel_default.append(panel_heading, collapse2);
-
-			//--------------------------
-			//First div of panel-default
-			panel_heading = panel_heading.append(panel_title);
-			panel_title = panel_title.append(collapse);
-			row = row.append(coursename,courseid);
-			collapse = collapse.append(row);
-
-			//--------------------------
-			//Second div of panel-default
-			collapse2 = collapse2.append(panelbody);
-
-			//DESCRIPTION
-			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
-										 	$("<div></div>", {class:"form-group col-sm-12"}).append(
-										 		description)));		//escaping closures
-
-			//STARTDATE & ENDDATE
-			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		startdate),			//escaping closures
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		enddate)));			//escaping closures
-
-			//COURSE TIME & COURSE INTERVAL
-			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		coursetime),			//escaping closures
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		courseinterval)));			//escaping closures
-
-			//COURSE COST & BUTTON
-			panelbody = panelbody.append($("<div></div>", {class: "row"}).append(
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		coursecost),			//escaping closures
-										 	$("<div></div>", {class:"form-group col-sm-6"}).append(
-										 		button)));			//escaping closures
-
-
-			courses.append(panel_default);
+		var courses = '';
+		courses += '<div id="scheduleaccordion" class="panel-group">';
+		for(i = 0; i < data.length; i++) {
+			courses += '    <div class="panel panel-primary">';
+			courses += '        <div class="panel-heading">';
+			courses += '            <h4 class="panel-title">';
+			courses += '                <a aria-expanded="false" class="collapsed" data-toggle="collapse" href="#collapse' + i + '">';
+			courses += '                    <div class="row">';
+			courses += '                        <div class="col-sm-5">' + data[i].course_name +'</div>';
+			courses += '                        <div class="col-sm-7 righttext">Course Code: ' + data[i].course_code + '</div>';
+			courses += '                    </div>';
+			courses += '                </a>';
+			courses += '            </h4>';
+			courses += '        </div>';
+			courses += '        <div style="height: 0px;" aria-expanded="false" class="panel-collapse collapse" id="collapse' + i + '">';
+			courses += '          	<div class="panel-body">';
+			courses += '        	<div class="col-sm-offset-1">';
+			courses += '        		<div class="row">';
+			courses += '            		<div class="form-group col-sm-12">';
+			courses += '                		<p id="description' + i + '">' + data[i].course_description + '</p>';
+			courses += '        			</div>';
+			courses += '        		</div>';			
+			courses += '        		<div class="row">';
+			courses += '            		<div class="col-sm-6">';
+			courses += '               		 	 <p id="coursestartdate' + i + '"><strong>Start Date: </strong>' + String(data[i].start_date).substring(0, 10) + '</p>';
+			courses += '            		</div>';
+			courses += '            		<div class="col-sm-6">';
+			courses += '                		  <p id="courseenddate' + i + '"><strong>End Date: </strong>' + String(data[i].end_date).substring(0, 10) + '</p>';
+			courses += '            		</div>';
+			courses += '        		</div>';
+			courses += '        		<div class="row">';
+			courses += '            		<div class="col-sm-6">';
+			courses += '                		<p id="coursetime' + i + '"><strong>Time: </strong>' + data[i].course_time + '</p>';
+			courses += '            		</div>';
+			courses += '            		<div class="col-sm-6">';
+			courses += '                  		<p id="courseinterval' + i + '"><strong>Interval: </strong>' + data[i].course_interval + '</p>';
+			courses += '           			</div>';
+			courses += '        		</div>';
+			courses += '        		<div class="row">';
+			//remove comment once target is linked
+			//courses += '        	    	<div class="col-sm-6">';
+			//courses += '        	        	 <p id="coursetarget' + i + '"><strong>Target: </strong>' + data[i].course_target + '</p>';
+			//courses += '         	   		</div>';
+			courses += '          	  		<div class="col-sm-6">';
+			courses += '         	         	<p id="cost' + i + '"><strong>Cost: </strong>' + data[i].default_fee + '</p>';
+			courses += '        	    	</div>';
+			courses += '        		</div>';
+			courses += '        		<div class="row">';
+			courses += '            		<div class="col-sm-6">';
+			courses += '            			<button type="submit" class="btn btn-default course-submit" onclick="register(this)" method="POST" id="submit" value="data[i].course_id">Register Now!!</button>';
+			courses += '            		</div>';
+			courses += '       			</div>';
+			courses += '        	</div>';
+			courses += '        	</div>';
+			courses += '        </div>';
+			courses += '    </div>';
 		}
-
+		schedule += '</div>';
 		$('#courses').append(courses);
 	});
 }
