28ec86f9f4ab5383a341adf13a063f13bc374093
Buraisx <situ_william@yahoo.com>
2016-03-25 23:08:52 -0400

    styled signup page with simple progressbar

---
+++ gibsonwebsite/public/js/signupdesign.js
@@ -0,0 +1,19 @@
+ $(document).ready(function(){
+    $('#add').click(function(){
+        $('#myCarousel').animate({height: '+=300'});
+    });
+    $('#remove').click(function(){
+        $('#myCarousel').animate({height: '-=300'});
+    });
+    $('#student').click(function(){
+        $('#myCarousel').animate({height: 1000});
+    });
+    $('#back').click(function(){
+    	$('#myCarousel').animate({height: 700});
+    });
+ });
+ window.onload = progress(33,$('#progressBar'));
+ function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    $element.find('div').animate({ width: progressBarWidth }, 500);
+}
