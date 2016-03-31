5a2d71111471e6e955f8ccf9ef1d044b78732f89
Timmaaaayy <timmaaayycheng@gmail.com>
2016-03-30 21:42:43 -0400

    navbar styling

---
+++ gibsonwebsite/public/js/login.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var divOne = document.getElementById('cart');
+    divOne.style.visibility = 'hidden';
+
+    var divTwo = document.getElementById('userdisplay');
+    divTwo.style.visibility = 'hidden';
+});
