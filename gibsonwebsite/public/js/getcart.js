d646a5b3f44e51dcaa31e23d561586f7ed0aec46
Benjamin Zhao <Benjamin.zhao1995@hotmail.com>
2016-03-31 21:21:20 -0400

    Courses in cart now appear in /cart

--- gibsonwebsite/public/js/getcart.js
+++ gibsonwebsite/public/js/getcart.js
@@ -6,5 +6,20 @@ $(document).ready(function(){
 function load_cart(){
 	jQuery.getJSON("/cart/view", function(course_info){
 		console.log(course_info);
+		//$('#cart-table').contents().remove();
+		
+		var cart_table = $('#cart-table');
+
+		for(var i = 0; i < course_info.length; i++){
+			var item='';
+			item+='<tr class="cart-item">';
+			item+='    <td class="cart-item-name">'+ course_info[i].course_name +'</td>';
+			item+='    <td class="cart-item-code">'+ course_info[i].course_code +'</td>';
+			item+='    <td class="cart-item-cost">$'+ course_info[i].default_fee +'</td>';
+			item+='    <td class="cart-delete">X</td>';
+			item+='</tr>';
+
+			cart_table.append(item);
+		}
 	});
 }
