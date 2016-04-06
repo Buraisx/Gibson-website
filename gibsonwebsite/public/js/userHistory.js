a06e3f7667f2def40815d44f7419aeeaa06e93d8
Leo <ienzoh@live.com>
2016-04-06 01:07:27 -0400

    Added account history table

--- gibsonwebsite/public/js/userHistory.js
+++ gibsonwebsite/public/js/userHistory.js
@@ -9,5 +9,38 @@ function load_history(){
 		_csrf: $('#_csrf').val()
 	}).done(function(res){
 		console.log(res);
+		$('#account-history').contents().remove();
+		var history = '';
+		var i;
+		for(i = 0; i < res.length; i++) {
+			var date = String(res[i].create_time).substring(0, 10); // Substring of valuable part
+			var state = res[i].state.charAt(0).toUpperCase() + res[i].state.slice(1); // Capitalizes first letter
+
+			history+='<table class = "table table-bordered">';
+			history+='    <thead>';
+			history+='        <tr>';
+			history+='            <th>Description</th>';
+			history+='            <th>Date</th>';
+			history+='            <th>Transaction ID</th>';
+			history+='            <th>Paypal ID</th>';
+			history+='            <th>State</th>';
+			history+='            <th>Payer Name</th>';
+			history+='            <th>Total</th>';
+			history+='        </tr>';
+			history+='    </thead>';
+			history+='    <tbody>';
+			history+='        <tr>';
+			history+='            <td class = "col-sm-4">' + res[i].description + '</td>';
+			history+='            <td class = "col-sm-2">' + date + '</td>';
+			history+='            <td>' + res[i].transaction_id + '</td>';
+			history+='            <td>' + res[i].paypal_id + '</td>';
+			history+='            <td>' + state + '</td>';
+			history+='            <td>' + res[i].payer_first_name + ' ' + res[i].payer_last_name + '</td>';
+			history+='            <td>' + res[i].total + '</td>';
+			history+='        </tr>';
+			history+='    </tbody>';
+			history+='</table>';
+		}
+		$('#account-history').append(history);
 	});
 }
