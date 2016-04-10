b521b93ecb251ce0073ea9966ce4dab2c421fba9
ienzoh <ienzoh@live.com>
2016-04-09 23:18:00 -0400

    Timepicki destroyed

--- gibsonwebsite/public/js/adminpanel.js
+++ gibsonwebsite/public/js/adminpanel.js
@@ -31,16 +31,6 @@ function controlpanel () {
     controlpanel+='            </div>';
     controlpanel+='        </div>';
     controlpanel+='        <div class = "row">';
-    controlpanel+='            <div class = "form-group col-sm-3">';
-    controlpanel+='                <label><span class="required">*</span>Start Time:</label>';
-    controlpanel+='                <input type = "text" class = "form-control timepicki" name = "starttime" id = "starttime" required>';
-    controlpanel+='            </div>';
-    controlpanel+='            <div class = "form-group col-sm-3">';
-    controlpanel+='                <label><span class="required">*</span>End Time:</label>';
-    controlpanel+='                <input type = "text" class = "form-control timepicki" name = "endtime" id = "endtime" required>';
-    controlpanel+='            </div>';
-    controlpanel+='        </div>';
-    controlpanel+='        <div class = "row">';
     controlpanel+='            <div class = "form-group col-sm-9">';
     controlpanel+='                <label><span class="required">*</span>Description:</label>';
     controlpanel+='                <textarea class = "form-control" rows = "3" name = "message" id = "message" required></textarea>';
