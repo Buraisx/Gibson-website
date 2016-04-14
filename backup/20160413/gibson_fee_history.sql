bc538c233c32ee7f7387d6215fb02ae32f110167
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-04-13 22:05:07 -0400

    New Database Change

---
+++ backup/20160413/gibson_fee_history.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: prod-gibson-instance.cdvppcqaisw3.us-west-2.rds.amazonaws.com    Database: gibson
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fee_history`
+--
+
+DROP TABLE IF EXISTS `fee_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fee_history` (
+  `fee_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_course_id` int(10) unsigned NOT NULL,
+  `status` varchar(45) NOT NULL DEFAULT 'pending',
+  `previous_balance` decimal(16,2) NOT NULL COMMENT 'Flat Number. If change is in percent please calculate amount beforehand',
+  `new_balance` decimal(16,2) NOT NULL COMMENT 'Flat Number. If change is in percent please calculate amount beforehand',
+  `balance_change` decimal(16,2) NOT NULL COMMENT 'Flat Number. If change is in percent please calculate amount beforehand',
+  `issue_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `notes` text,
+  PRIMARY KEY (`fee_history_id`),
+  UNIQUE KEY `fee_history_id_UNIQUE` (`fee_history_id`),
+  KEY `user_course_id` (`user_course_id`),
+  CONSTRAINT `enrollment_id` FOREIGN KEY (`user_course_id`) REFERENCES `user_course` (`enrollment_id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-13 21:33:14
