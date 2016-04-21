c71bebbe63207cdfdca621132b4fe5b79a8dbefd
Kevin Xu <kevinxu.444@gmail.com>
2016-04-20 23:46:34 -0400

    fixed query error

--- gibsonwebsite/authentication/passport.js
+++ gibsonwebsite/authentication/passport.js
@@ -122,9 +122,9 @@ module.exports = function(passport){
                 newUser.province_id = req.body.province;
                 newUser.postal_code = req.body.postal_code.toUpperCase().replace(/ /g,''); //Deletes the white space in postal code.
                 newUser.primary_phone = req.body.primary_phone.replace(/\D+/g, ''); //Removes everything, but digits [0-9]
-                newUser.primary_extension = req.body.primary_extension.replace(/\D+/g, '');
+                newUser.primary_extension = (!req.body.primary_extension)? null:req.body.primary_extension.replace(/\D+/g, '');
                 newUser.secondary_phone = req.body.secondary_phone.replace(/\D+/g, '');
-                newUser.secondary_extension = req.body.secondary_extension.replace(/\D+/g, '');
+                newUser.secondary_extension = (!req.body.secondary_extension)? null:req.body.secondary_extension.replace(/\D+/g, '');
                 newUser.email = req.body.email;
                 newUser.send_notification = (!req.body.send_notifications)? 0:req.body.send_notifications;
                 newUser.student = (!req.body.student)? 0:req.body.student;
@@ -133,7 +133,7 @@ module.exports = function(passport){
                 var createUser  = 'INSERT INTO gibson.temp_user (rank_id, type, username, password, lname, fname, birth_date, gender, address, ';
                     createUser +=                               'unit_no, city, province_id, postal_code, primary_phone, primary_extension, ';
                     createUser +=                               'secondary_phone, secondary_extension, email, send_notification, student) ';
-                    createUser += 'VALUES(1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
+                    createUser += 'VALUES(1, "REGULAR", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
                 var values = [
                   newUser.username, newUser.password, newUser.lname, newUser.fname, newUser.birth_date, newUser.gender, newUser.address,
                   newUser.unit_no, newUser.city, newUser.province_id, newUser.postal_code, newUser.primary_phone, newUser.primary_extension,
