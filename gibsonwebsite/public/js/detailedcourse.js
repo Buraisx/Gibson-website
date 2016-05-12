811815918c80ef279b8675bf47a8fe1400c1dac8
Benjamin Zhao <Benjamin.zhao1995@hotmail.com>
2016-05-12 14:44:32 -0400

    Added edit button feature to show search bar. Search bar added with autocomplete of list of all tags

--- gibsonwebsite/public/js/detailedcourse.js
+++ gibsonwebsite/public/js/detailedcourse.js
@@ -9,16 +9,30 @@ $(document).ready(getCourseTags());
 
 function getCourseTags() {
 	jQuery.getJSON("/staff/portal/detailedcourse/data",{course: getParameterByName('course')}, function(data){
-		var course_tags = $('#sortable-tags')
+		var course_tags = $('#tags-sortable');
+		var tag_list = [];
+
 		//***// 
 		//data[0].course holds all the course info
 		//data[0].categories holds a list of course categories
 		//data[1] holds a list of all existing category types
 		//***//
+		
+		//Generate tag list
 		for(var i = 0; i < data[1].length; i++){
+			tag_list.push(data[1].category_string);
+		}
+		$('tags-search').autocomplete({tag_list});
+
+		//Generate Draggable list of course's tags
+		for(var i = 0; i < data[0].categories.length; i++){
 			course_tags.append('<li class="ui-state-default" value=' + data[0].categories[i].category_id + '>' + data[0].categories[i].category_string + '</li>');
 		}
 	});
+}
+
+function editMode(){
+	$('tags-search').show();
 }
 
 function addedTag(){
