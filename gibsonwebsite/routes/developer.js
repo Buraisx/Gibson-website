5d4f74672a0f4a109cae743212bb050fe33a1a9c
Kevin Xu <kevinxu.444@gmail.com>
2016-05-12 23:32:02 -0400

    convert limited user function fix

--- gibsonwebsite/routes/developer.js
+++ gibsonwebsite/routes/developer.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var config = require('../server_config');
 var jwt = require('jsonwebtoken');
 
-router.get('/test_profile', function(req, res) {
-      res.render('test_profile', {title: 'User Profile'});
+router.get('/developer', function(req, res) {
+    res.render('developer', {title: 'LEL'});
 });
 
 module.exports = router;
