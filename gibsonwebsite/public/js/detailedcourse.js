6633dfad75a3970cfed2509260c90c84f4d7c921
Benjamin Zhao <Benjamin.zhao1995@hotmail.com>
2016-05-12 20:47:41 -0400

    Added a add tag bar and autofill is working. Need styling on draggables and autofill dropdown

--- gibsonwebsite/public/js/detailedcourse.js
+++ gibsonwebsite/public/js/detailedcourse.js
@@ -1,5 +1,10 @@
+//*** GLOBAL VARIABLES ***//
+var TAG_LIST=[];	//LIST OF ALL TAGS 
+var COURSE_TAG_LIST=[]; //LIST OF ALL COURSE TAGS
+//***********************//
+
 $(function() {
-	$( "#sortable-tags" ).sortable({ 
+	$( "#tags-sortable" ).sortable({ 
 		placeholder: "ui-sortable-placeholder" 
 	});
 });
@@ -9,8 +14,8 @@ $(document).ready(getCourseTags());
 
 function getCourseTags() {
 	jQuery.getJSON("/staff/portal/detailedcourse/data",{course: getParameterByName('course')}, function(data){
-		var course_tags = $('#tags-sortable');
-		var tag_list = [];
+		var TAG_LIST = data[1];
+		var COURSE_TAG_LIST = data[0].categories;
 
 		//***// 
 		//data[0].course holds all the course info
@@ -18,21 +23,51 @@ function getCourseTags() {
 		//data[1] holds a list of all existing category types
 		//***//
 		
-		//Generate tag list
-		for(var i = 0; i < data[1].length; i++){
-			tag_list.push(data[1].category_string);
-		}
-		$('tags-search').autocomplete({tag_list});
-
-		//Generate Draggable list of course's tags
-		for(var i = 0; i < data[0].categories.length; i++){
-			course_tags.append('<li class="ui-state-default" value=' + data[0].categories[i].category_id + '>' + data[0].categories[i].category_string + '</li>');
-		}
+		updateAutoCompleteList(data[1]);
+		updateCourseTagsList(data[0].categories);
 	});
 }
 
+
+function updateAutoCompleteList(data){
+	var autofill_list = [];
+
+	//Generate tag list
+	for(var i = 0; i < data.length; i++){
+		autofill_list.push(data[i].category_string);
+	}
+	
+	$('#tags-search').autocomplete({
+		source: autofill_list,
+		messages: {
+        	noResults: '',
+        	results: function() {}
+    	}
+	});
+}
+
+function updateCourseTagsList(data){
+	var course_tags = $('#tags-sortable');
+	course_tags.empty();
+
+	//Generate Draggable list of course's tags
+	for(var i = 0; i < data.length; i++){
+		course_tags.append('<li class="ui-state-default" value=' + data[i].category_id + '>' + data[i].category_string + '</li>');
+	}
+}
+
+function addTag(category_string){
+	for(var i = 0; i < TAG_LIST.length; i++){
+		if(TAG_LIST[i].category_string === category_string){
+			//*** UPDATE COURSE TAGS IN DATABASE ***//
+		}
+	}
+}
+
 function editMode(){
-	$('tags-search').show();
+	$('#tags-search').show();
+	$('#tags-search-divider').show();
+	$('#tags-add').show();
 }
 
 function addedTag(){
