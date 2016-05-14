6d3a29f0c07d848cb6219661f66370f5e4366a33
Allen Lee <allenyuklun.lee@mail.utoronto.ca>
2016-05-14 17:12:45 -0400

    Bad connection created

--- gibsonwebsite/mysqlpool.js
+++ gibsonwebsite/mysqlpool.js
@@ -2,9 +2,19 @@ var mysql = require("mysql");
 var config = require("./server_config");
 
 var connection = mysql.createPool(config.db_config);
+var badConnection = mysql.createPool(config.db_badconfig);
 
 exports.getConnection = function (callback){
   connection.getConnection(function(err, con){
+    if (err){
+      return callback(err);
+    }
+    callback(err, con);
+  });
+};
+
+exports.getBadConnection = function (callback){
+  badConnection.getConnection(function(err, con){
     if (err){
       return callback(err);
     }
